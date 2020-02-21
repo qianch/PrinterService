bf35985bb1e5bcfa26429f180df09a45cc3e2fe0
qianchenchn <qianchenchn@foxmail.com>
2020-02-21 10:58:16 +0800

    bufferSize调整为100KB

--- PrinterService/PrinterService.cs
+++ PrinterService/PrinterService.cs
@@ -33,7 +33,7 @@ namespace PrinterService
 
         private Thread skThread = null;
 
-        private int bufferSize = 1024 * 8 * 20; //20KB
+        private int bufferSize = 1024 * 8 * 100; //100KB
 
         private long countAll = 0L;
 
